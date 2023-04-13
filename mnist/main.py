8d7303e90a4b0937ac2561661912a3ff7074f400
hotgaecha <qqrx224@naver.com>
2023-04-13 08:59:39 +0000

    added missing non-linearity

--- mnist/main.py
+++ mnist/main.py
@@ -25,7 +25,7 @@ class Net(nn.Module):
         x = F.max_pool2d(x, 2)
         x = self.dropout1(x)
         x = torch.flatten(x, 1)
-        x = self.fc1(x)
+        x = self.fc1(x)       
         x = F.relu(x)
         x = self.dropout2(x)
         x = self.fc2(x)
